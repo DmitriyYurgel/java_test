93dae065d7a891cbe60e22d8ba7c61f89a1a92ac
DmitriyYurgel <48963748+DmitriyYurgel@users.noreply.github.com>
2021-03-17 21:12:18 +0300

    Изменилось название класса

--- sandbox/src/main/java/ru/stqa/pft/sandbox/newname.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/newname.java
@@ -1,7 +1,7 @@
 package ru.stqa.pft.sandbox;
 
-public class newname {
-	public static void main(String[] args){
-		System.out.println("Hello, world!");
-	}
-} 
+public class Newname {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
