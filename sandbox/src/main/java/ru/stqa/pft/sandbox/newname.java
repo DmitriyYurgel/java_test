9f2d5370f8576a32403156259b04e204bf97baba
DmitriyYurgel <48963748+DmitriyYurgel@users.noreply.github.com>
2021-03-18 14:05:35 +0300

    Переименование файла

--- sandbox/src/main/java/ru/stqa/pft/sandbox/newname.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/newname.java
@@ -3,8 +3,5 @@ package ru.stqa.pft.sandbox;
 public class Newname {
   public static void main(String[] args) {
     System.out.println("Hello, world!");
-
-    System.out.println(2 + 2);
-    System.out.println("Площадь квадрата со строной " + 5 + "=" + (5*5));
   }
 }
